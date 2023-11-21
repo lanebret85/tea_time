b7a0fd3bc48073aa5f36b08c4c89f10301dd5c7b
Lane Bretschneider <lanebret85@gmail.com>
2023-11-21 11:36:43 -0800

    test: add model test including validations and relationships

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Customer, type: :model do
+  describe "relationships" do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:subscription_teas).through(:subscriptions) }
+    it { should have_many(:teas).through(:subscription_teas) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+  end
+end

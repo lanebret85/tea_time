55fc577c7c67440d25c458aac395b28a5540f889
Lane Bretschneider <lanebret85@gmail.com>
2023-11-21 13:26:02 -0800

    refactor: modify format for enums

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
-  enum :status, {ordered: 0, shipped: 1, delivered: 2}
-  enum :frequency, {weekly: 0, monthly: 1, quarterly: 2}
+  enum status: { ordered: 0, shipped: 1, delivered: 2 }
+  enum frequency: { weekly: 0, monthly: 1, quarterly: 2 }
 
   belongs_to :customer
   has_many :subscription_teas

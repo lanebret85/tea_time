61b2b1c6a28b93f224ca134a52aef7676e8e0258
Lane Bretschneider <lanebret85@gmail.com>
2023-11-23 14:43:22 -0800

    feat: add route for get '/api/v1/customers/subscriptions'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/api/v1/customers/subscriptions", to: "api/v1/customers/subscriptions#index"
   post "/api/v1/customers/subscriptions", to: "api/v1/customers/subscriptions#create"
   patch "/api/v1/customers/subscriptions", to: "api/v1/customers/subscriptions#update"
 end
